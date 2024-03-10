fbd2e44737041bc98905c35b60d7200a5d60274b
Nicolas Souza <nicolas_s_de-souza@estudante.sesisenai.org.br>
2024-03-10 19:32:52 -0300

    [M1S08] - Ex 3 - Criar e Adicionar Elemento

--- Exercicios 11-03-2024 DOM/script.js
+++ Exercicios 11-03-2024 DOM/script.js
@@ -1,8 +1,22 @@
+//-------------Exercicío 1----------------------- 
+
 document.getElementById('titulo').style.backgroundColor = 'red';
+
+//-------------Exercicío 2----------------------- 
 
 let spans = document.getElementsByClassName('span');
 
 for (let i = 0; i < spans.length; i++) {
     spans[i].innerText = 'Texto alterado em todos os spans a partir de uma manipulação de DOM'
 } 
+
+//-------------Exercicío 3----------------------- 
+
+let content = document.getElementById('content');
+
+let elemento = document.createElement('span');
+
+elemento.appendChild(document.createTextNode('Criei meu span utilizando manipulação de DOM'));
+
+content.appendChild(elemento);
 
