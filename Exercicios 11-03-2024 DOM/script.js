96842dfb4bb8e3d77ef47dce233fe59e578a8527
Nicolas Souza <nicolas_s_de-souza@estudante.sesisenai.org.br>
2024-03-10 19:15:04 -0300

    [M1S08] - Ex 2 - Selecione os elementos HTML por sua classe

--- Exercicios 11-03-2024 DOM/script.js
+++ Exercicios 11-03-2024 DOM/script.js
@@ -1,1 +1,8 @@
 document.getElementById('titulo').style.backgroundColor = 'red';
+
+let spans = document.getElementsByClassName('span');
+
+for (let i = 0; i < spans.length; i++) {
+    spans[i].innerText = 'Texto alterado em todos os spans a partir de uma manipulação de DOM'
+} 
+
