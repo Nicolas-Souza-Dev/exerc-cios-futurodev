89095d10476942bcd17d7a4d9e4bbbabfb32d9b5
Nicolas Souza <nicolas_s_de-souza@estudante.sesisenai.org.br>
2024-03-19 21:43:25 -0300

    [M1S08] - Ex 10 - Lista de tarefas

--- Exercicios 11-03-2024 DOM/script.js
+++ Exercicios 11-03-2024 DOM/script.js
@@ -1,39 +1,39 @@
 //-------------Exercicío 1----------------------- 
 
-document.getElementById('titulo').style.backgroundColor = 'red';
+// document.getElementById('titulo').style.backgroundColor = 'red';
 
 //-------------Exercicío 2----------------------- 
 
-let spans = document.getElementsByClassName('span');
+// let spans = document.getElementsByClassName('span');
 
-for (let i = 0; i < spans.length; i++) {
-    spans[i].innerText = 'Texto alterado em todos os spans a partir de uma manipulação de DOM'
-} 
+// for (let i = 0; i < spans.length; i++) {
+//     spans[i].innerText = 'Texto alterado em todos os spans a partir de uma manipulação de DOM'
+// } 
 
 //-------------Exercicío 3----------------------- 
 
-let content = document.getElementById('content');
+// let content = document.getElementById('content');
 
-let elemento = document.createElement('span');
+// let elemento = document.createElement('span');
 
-elemento.appendChild(document.createTextNode('Criei meu span utilizando manipulação de DOM'));
+// elemento.appendChild(document.createTextNode('Criei meu span utilizando manipulação de DOM'));
 
-content.appendChild(elemento);
+// content.appendChild(elemento);
 
 
 //-------------Exercicío 4----------------------- 
 
-let remove = document.querySelector('#content span');
-remove.remove();
+// let remove = document.querySelector('#content span');
+// remove.remove();
 
 //-------------Exercicío 5----------------------- 
 
-let classes = document.getElementsByClassName('span');
+// let classes = document.getElementsByClassName('span');
 
 
-for (let i = 0; i < classes.length; i++) {
-    classes[i].style.backgroundColor = 'blue';
-}
+// for (let i = 0; i < classes.length; i++) {
+//     classes[i].style.backgroundColor = 'blue';
+// }
 
 
  //-------------Exercicío 6----------------------- 
@@ -54,17 +54,43 @@ for (let i = 0; i < classes.length; i++) {
 
    //-------------Exercicío 8----------------------- 
 
-let myelemento = document.getElementById('meuElemento');
+// let myelemento = document.getElementById('meuElemento');
 
-myelemento.setAttribute("data-atual", "13/03/2024")
+// myelemento.setAttribute("data-atual", "13/03/2024")
 
-console.log(myelemento)
+   //-------------Exercicío 9 ----------------------- 
 
-   //-------------Exercicío 9----------------------- 
+  //  myelemento.removeAttribute("data-atual");
 
-   myelemento.removeAttribute("data-atual");
+  //  console.log(myelemento)
 
-   console.log(myelemento)
+    //-------------Exercicío 10 ----------------------- 
+
+    let contadorTarefa = 1;
+
+    function adicionarTarefa() {
+      let tarefa = document.getElementById("inputTarefa").value;
+
+      if (tarefa !== "") {
+        let lista = document.querySelector(".listaTarefas");
+        let li = document.createElement('li');
+        li.innerHTML = `${tarefa} <button onclick = "removerTarefa(this)" >Remover</button>`;
+        li.id = `t${contadorTarefa}`;
+        lista.appendChild(li);
+        contadorTarefa++;
+        document.getElementById("inputTarefa").value = "";
+      } else {
+        alert("Por favor, digite uma tarefa válida.")
+      }
+    }
+
+    function removerTarefa(element) {
+      element.parentNode.remove();
+    }
+
+
+
+
 
 
 
